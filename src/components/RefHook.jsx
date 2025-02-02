dbb13343358b6d8b80129fc498a009187e9f2d36
Kalidass <kalidass.k021@gmail.com>
2025-02-02 12:40:03 +0530

    accessing the dom using use ref hook

--- src/components/RefHook.jsx
+++ src/components/RefHook.jsx
@@ -10,6 +10,8 @@ const RefHook = () => {
   const [value, setValue] = useState(0);
   //   const [count, setCount] = useState(0);
   const count = useRef(0);
+  // access the dom element using useRef
+  const inputElem = useRef();
 
   //   useEffect(() => {
   //     setCount(prev => prev + 1);
@@ -18,12 +20,23 @@ const RefHook = () => {
   useEffect(() => {
     count.current += 1;
   })
+
+  const handleClick = () => {
+    console.log('input', inputElem.current)
+    inputElem.current.style.backgroundColor = 'red';
+  }
   return (
     <div>
       <button onClick={() => setValue((prev) => prev - 1)}>-1</button>
       <h1>{value}</h1>
       <button onClick={() => setValue((prev) => prev + 1)}>+1</button>
       <h2>Render Count: {count.current}</h2>
+
+      <div>
+        {/* accessing the dom using useRef */}
+        <input type="text" ref={inputElem} />
+        <button onClick={handleClick} >Click</button>
+      </div>
     </div>
   );
 };
